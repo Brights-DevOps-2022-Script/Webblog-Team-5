1f36024ec5f7a492d2d0b02a0e7dc9d452b4f8aa
halim-aj <halim.allemagne@gmail.com>
2023-01-18 09:47:30 +0100

    UI commit

--- src/de/ithoc/frontend/UserInterface.java
+++ src/de/ithoc/frontend/UserInterface.java
@@ -1,5 +1,37 @@
 package de.ithoc.frontend;
+import java.util.Scanner;
 
-public class UserInterface{
-    
+public class UserInterface {
+
+    public class AttributeScanner {
+        private String name;
+        private String id;
+
+    public void scanAttributeName() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        name = scan.nextLine();
+    }
+
+    public void scanAttributeId() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your ID: ");
+        id = scan.nextLine();
+    }
+    public String getAttributeName() {
+        return name;
+    }
+
+    public void setattributeName(String name) {
+        this.name = name;
+    }
+
+    public String getattributeId() {
+        return id;
+    }
+
+    public void setattributeValue(String id) {
+        this.id = id;
+    }
 }
+    
