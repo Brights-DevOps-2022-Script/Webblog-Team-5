748c9caca23002f82f544e2821068b0f43c289eb
halim-aj <halim.allemagne@gmail.com>
2023-01-19 10:15:26 +0100

    user class attributes , getter und setter

--- src/de/ithoc/backend/User.java
+++ src/de/ithoc/backend/User.java
@@ -1,5 +1,36 @@
 package de.ithoc.backend;
+//import java.util.ArrayList;
 
 public class User {
+
+    private String userName;
+    private String passwort;
+    
+    User user = new User();
+
+    public void setUsername(String username) {
+        this.userName = username;
+    }
+
+    public String getUsername() {
+        return userName;
+    }
+
+    public void setPassword(String passwort) {
+        this.passwort = passwort;
+    }
+
+    public String getPasswort() {
+        return passwort;
+    }
+    public void editOwnPost(int postId, String newContent) {
+        
+        Post post = dataStorage.getPost(postId);
+        if(post.getAuthor().equals(this.username)) {
+            post.methodToChangeTheContent(newContent);
+            dataStorage.updatePost(post);
+        }
+
+
     
 }
