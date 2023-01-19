38fd835afaaf0ca31b17a92b63a89f5d1fa46b1e
UlrikeKlante <ulrikeklante@gmail.com>
2023-01-19 10:01:55 +0100

    comment

--- src/de/ithoc/backend/Comment.java
+++ src/de/ithoc/backend/Comment.java
@@ -1,5 +1,57 @@
 package de.ithoc.backend;
 
+import java.util.Date;
+
 public class Comment {
-    
+
+// Attributes
+
+    private int commentId;
+    private String text;
+    private String title;
+    private String author;
+    private Date date;
+
+// Methods
+
+    public void setId(int commentId){
+        this.commentId = commentId;         // Impelementieren: fortlaufend, also +1
+    }
+
+    public int getId(){
+     return commentId;   
+    }
+
+    public void setText(String text){
+        this.text = text;
+    }
+
+    public String getText(){
+        return text;
+    }
+
+    public void setTitle(String title){
+        this.title=title;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public void setAuthor(String author){
+        this.author=author;
+    }
+
+    public String getAuthor(){
+        return author;
+    }
+
+    public void setDate(Date date){
+        this.date=date;
+    }
+
+    public Date getDate(){
+        return date;
+    }
+
 }
